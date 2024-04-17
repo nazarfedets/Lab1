da58fc54147ebb7509c9be1036524a596b6cb67a
nazarfedets <160725999+nazarfedets@users.noreply.github.com>
2024-04-17 19:50:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 6;
+        System.out.println((double)a / (double)b);
+        System.out.println((double)a % (double)b);
+
+        int c = 25;
+        int d = c / 10 + c % 10;
+        System.out.println(d);
+
+        double r = 2.5;
+        double e = r + 0.5;
+        System.out.println((int)e);
+
+    }
+
+}
